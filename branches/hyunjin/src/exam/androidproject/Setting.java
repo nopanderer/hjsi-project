4c53ba6130882080a327c28560684eea42986658
hyunjin7857 <hyunjin7857@bde29758-66fb-aabd-645c-96a9e71e0505>
2014-11-09 17:06:09 +0000

    141110

--- branches/hyunjin/src/exam/androidproject/Setting.java
+++ branches/hyunjin/src/exam/androidproject/Setting.java
@@ -5,13 +5,12 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 
-public class Setting extends Dialog implements OnTouchListener
+public class Setting extends Dialog implements OnClickListener
 {
     private Button resume, help, sound, credits, quit;
 
@@ -22,10 +21,11 @@ public class Setting extends Dialog implements OnTouchListener
 
     protected void onCreate(Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
+
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_setting);
 
         resume = (Button) findViewById(R.id.set_resume_btn);
@@ -34,22 +34,21 @@ public class Setting extends Dialog implements OnTouchListener
         credits = (Button) findViewById(R.id.set_credits_btn);
         quit = (Button) findViewById(R.id.set_quit_btn);
 
-        resume.setOnTouchListener(this);
-        quit.setOnTouchListener(this);
+        resume.setOnClickListener(this);
+        quit.setOnClickListener(this);
     }
 
-    public boolean onTouch(View v, MotionEvent event)
+    public void onClick(View v)
     {
         if (v == resume)
         {
-            cancel();
+            hide();
         }
         else if (v == quit)
         {
             dismiss();
         }
 
-        return false;
     }
 
 }
