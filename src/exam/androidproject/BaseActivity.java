d0872eee22a8b2538f2707d725fa6516e6b1817e
이상인 <@bde29758-66fb-aabd-645c-96a9e71e0505>
2014-11-18 16:12:49 +0000

    [trunk] merge from [hyunjin]

---
+++ src/exam/androidproject/BaseActivity.java
@@ -0,0 +1,11 @@
+package exam.androidproject;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+
+public class BaseActivity extends Activity
+{
+    public static ArrayList<Activity> actList = new ArrayList<Activity>();
+
+}
--- src/exam/androidproject/Setting.java
+++ src/exam/androidproject/Setting.java
@@ -5,15 +5,16 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
-public class Setting extends Dialog implements OnTouchListener
+public class Setting extends Dialog implements OnClickListener
 {
-    private Button resume, help, sound, credits, quit;
+    private Button       resume, help, credits, quit;
+    private ToggleButton sound;
 
     public Setting(Context context)
     {
@@ -22,34 +23,51 @@ public class Setting extends Dialog implements OnTouchListener
 
     protected void onCreate(Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
+
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_setting);
 
         resume = (Button) findViewById(R.id.set_resume_btn);
         help = (Button) findViewById(R.id.set_help_btn);
-        sound = (Button) findViewById(R.id.set_sound_btn);
+        sound = (ToggleButton) findViewById(R.id.set_sound_btn);
         credits = (Button) findViewById(R.id.set_credits_btn);
         quit = (Button) findViewById(R.id.set_quit_btn);
 
-        resume.setOnTouchListener(this);
-        quit.setOnTouchListener(this);
+        resume.setOnClickListener(this);
+        sound.setOnClickListener(this);
+        quit.setOnClickListener(this);
     }
 
-    public boolean onTouch(View v, MotionEvent event)
+    public void onClick(View v)
     {
         if (v == resume)
         {
-            cancel();
+            hide();
+        }
+        else if (v == sound)
+        {
+            if (sound.isChecked())
+            {
+                sound.setBackgroundDrawable(sound.getResources().getDrawable(R.drawable.set_soundoff_img));
+                Map.music.pause();
+            }
+            else
+            {
+                sound.setBackgroundDrawable(sound.getResources().getDrawable(R.drawable.set_soundon_img));
+                Map.music.start();
+            }
         }
         else if (v == quit)
         {
+            Map.music.stop();
+            Map.music.release();
+            Map.music = null;
             dismiss();
         }
 
-        return false;
     }
 
 }
