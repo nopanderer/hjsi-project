0c9403c3c991c954b3371966ae0200cc3d377e69
hyunjin7857 <hyunjin7857@bde29758-66fb-aabd-645c-96a9e71e0505>
2015-01-23 16:18:15 +0000

    make branch

---
+++ branches/20150124_hj/src/hjsi/activity/DlgStore.java
@@ -0,0 +1,95 @@
+package hjsi.activity;
+
+import hjsi.common.AppManager;
+import hjsi.customview.TowerView;
+import hjsi.game.Tower;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.TextView;
+
+public class DlgStore extends Dialog implements OnClickListener {
+
+  private View page1;
+  private View page2;
+
+  public DlgStore(Context context) {
+    super(context);
+  }
+
+  public DlgStore(Context context, int theme) {
+    super(context, theme);
+  }
+
+  public DlgStore(Context context, boolean cancelable, OnCancelListener cancelListener) {
+    super(context, cancelable, cancelListener);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    AppManager.printSimpleLog();
+    super.onCreate(savedInstanceState);
+
+    requestWindowFeature(Window.FEATURE_NO_TITLE); // 다이얼로그 제목 표시를 없앤다
+    getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // 다이얼로그 배경을 없앤다
+    // (테두리도 없음)
+    setContentView(R.layout.dialog_store);
+
+    // 구매를 취소하거나 구매 후 "계속 구매", "배치"도 누르지 않는 경우를 위한 리스너
+    setOnCancelListener(new OnCancelListener() {
+      @Override
+      public void onCancel(DialogInterface dialog) {
+        // 구매 버튼을 다시 누를 수 있게 팝업에서 표시할 창을 바꿔주는 것
+        page1.setVisibility(View.VISIBLE);
+        page2.setVisibility(View.INVISIBLE);
+      }
+    });
+
+    page1 = findViewById(R.id.dlg_store_goods_page1); // 원소 구매 확인창
+    page2 = findViewById(R.id.dlg_store_goods_page2); // 구매한 원소 표시 및 계속 구매할지 배치할지 묻는 창
+
+    // 각 버튼들(두 개의 창에 두 개의 버튼)에 리스너를 등록한다.
+    ((TextView) findViewById(R.id.dlg_store_btn_buy)).setOnClickListener(this);
+    ((TextView) findViewById(R.id.dlg_store_btn_cancel)).setOnClickListener(this);
+    ((TextView) findViewById(R.id.dlg_store_btn_morebuy)).setOnClickListener(this);
+    ((TextView) findViewById(R.id.dlg_store_btn_arrangement)).setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    Log.d(toString(), AppManager.getMethodName() + "() " + v.toString());
+
+    switch (v.getId()) {
+      case R.id.dlg_store_btn_buy: // 정말로 원소 구매할지 확인하는 창의 구매 버튼
+        page1.setVisibility(View.INVISIBLE); // 현재 창을 숨기고
+        page2.setVisibility(View.VISIBLE); // 다음 창을 보여줌
+
+        // TODO 구매 관련 메소드를 작성한다
+
+        // 테스트용으로 기본 element 만들어서 뷰에 설정한다.
+        TowerView ev = (TowerView) findViewById(R.id.dlg_store_goods_element);
+        ev.setElement(new Tower());
+        ev.changeDetailLevel(1);
+        break;
+
+      case R.id.dlg_store_btn_cancel: // 취소 버튼을 누르는 경우
+        hide();
+        break;
+
+      case R.id.dlg_store_btn_morebuy: // 더 구매하려는 경우
+        // TODO 위의 element 구매 관련 메소드를 실행하면 될듯.
+        break;
+
+      case R.id.dlg_store_btn_arrangement: // 구매한 원소를 바로 배치하려는 경우다
+        // TODO 현재 구매해서 나온 원소를 맵에 바로 배치할 수 있게 처리한다
+        break;
+    }
+  }
+}
---
+++ branches/20150124_hj/src/hjsi/activity/Loader.java
@@ -0,0 +1,89 @@
+package hjsi.activity;
+
+import hjsi.common.AppManager;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Loader extends Base {
+  AnimationDrawable mAni;
+
+  Runnable loadingMethod = new Runnable() {
+    @Override
+    public void run() {
+      try {
+        /*
+         * 저장된 게임 진행 상태를 먼저 불러온다. 진행 상태를 토대로 앞으로 필요한 각종 리소스를 불러온다.
+         */
+
+
+
+        /*
+         * 공통적인 리소스를 불러온다.
+         */
+    	  // TODO 비트맵 리소스 읽어오는 걸 텍스트 파일에서 목록을 받아와서 반복문으로 불러오자
+        Bitmap imgRes = BitmapFactory.decodeResource(getResources(), R.drawable.img_tempmap);
+        AppManager.getInstance().addBitmap("background", imgRes);
+
+        Options opt = new Options();
+        opt.outWidth = 80;
+        opt.outHeight = 100;
+        imgRes = BitmapFactory.decodeResource(getResources(), R.drawable.img_unit_statue_owl, opt);
+        AppManager.getInstance().addBitmap("statue", imgRes);
+
+        Thread.sleep(2000); // 여기서 로딩 작업을 한다고 치고..
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      mHandler.sendEmptyMessage(0);
+    }
+  };
+
+  Handler mHandler = new Handler() {
+    @Override
+    public void handleMessage(Message msg) {
+      if (msg.what == 0) {
+        mAni.stop();
+        mAni = null;
+
+        setResult(Activity.RESULT_OK);
+        finish();
+        AppManager.printSimpleLog();
+      }
+    }
+
+  };
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    AppManager.printSimpleLog();
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_continue);
+
+    String[] strHelp = getResources().getStringArray(R.array.help_text);
+    TextView helpTextView = (TextView) findViewById(R.id.helpTextView);
+    helpTextView.setText(strHelp[0]);
+
+    Thread worker = new Thread(loadingMethod);
+
+    LinearLayout loading = (LinearLayout) findViewById(R.id.loading);
+    mAni = (AnimationDrawable) loading.getBackground();
+    mAni.setOneShot(false);
+    mAni.start();
+    worker.start();
+  }
+
+  @Override
+  public void onBackPressed() {
+    // Back 키 눌러도 동작하지 않도록 아무것도 하지 않음. (로그만 출력)
+    AppManager.printSimpleLog();
+  }
+}
