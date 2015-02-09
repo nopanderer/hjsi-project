521ff82adecb8c468e966585bc19616484fb5c8f
hyunjin7857 <hyunjin7857@bde29758-66fb-aabd-645c-96a9e71e0505>
2015-02-09 09:26:47 +0000

    [branches/0206_hj] Projectile extends Unit

--- src/hjsi/game/Projectile.java
+++ src/hjsi/game/Projectile.java
@@ -1,8 +1,6 @@
 package hjsi.game;
 
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 
 /**
  * 투사체
@@ -10,108 +8,121 @@ import android.graphics.Paint;
  * @author Administrator
  *
  */
-public class Projectile {
+public class Projectile extends Unit implements Movable
+{
 
-  /**
-   * 비트맵을 그려주기 위한 좌표
-   */
-  private int x, y;
-  private int target;
-  /**
-   * 투사체 속도
-   */
-  private int speed;
-  public boolean isHit;
-  /**
-   * 임시 타이머 변수
-   */
-  private long beforeTime;
-  private int sleep = 15;
-  /**
-   * 비트맵 크기
-   */
-  public int width, height;
-  public Bitmap face;
-  public Paint paint;
+    /**
+     * 목표로 한 몹의 인덱스
+     */
+    private int    target;
+    /**
+     * 투사체 속도
+     */
+    private int    speed;
+    /**
+     * 몹과 충돌했는지
+     */
+    public boolean isHit;
+    /**
+     * 임시 타이머 변수
+     */
+    private long   beforeTime;
+    private int    sleep = 15;
 
-  public Projectile(int x, int y, int target, Bitmap face) {
-    this.x = x;
-    this.y = y;
-    this.target = target;
-    width = face.getWidth();
-    height = face.getHeight();
-    face = this.face = face;
-    paint = new Paint();
+    /**
+     * 비트맵 크기
+     */
 
-    speed = 3;
-    isHit = false;
+    public Projectile(int x, int y, int target, Bitmap face)
+    {
+        super(x, y, face);
+        this.target = target;
 
-    beforeTime = System.currentTimeMillis();
-  }
+        speed = 3;
+        isHit = false;
 
-  public void move() {
-    // GameState.getInstance().Mobs.get(target).
-
-    if (System.currentTimeMillis() - beforeTime > sleep)
-      beforeTime = System.currentTimeMillis();
-    else
-      return;
-
-    if ((x >= targetX() && x <= targetXWidth()) && y >= targetY() && y <= targetYHeight())
-      isHit = true;
-
-    if (targetCntrX() < x) {
-      x -= speed;
-      if (targetCntrY() < y)
-        y -= speed;
-      else if (targetCntrY() > y)
-        y += speed;
-    }
-    if (targetCntrX() > x) {
-      x += speed;
-      if (targetCntrY() < y)
-        y -= speed;
-      else if (targetCntrY() > y)
-        y += speed;
-    }
-    if (targetCntrX() == x) {
-      if (targetCntrY() < y)
-        y -= speed;
-      else if (targetCntrY() > y)
-        y += speed;
+        beforeTime = System.currentTimeMillis();
     }
 
-  }
+    @Override
+    public void move()
+    {
+        if (System.currentTimeMillis() - beforeTime > sleep)
+            beforeTime = System.currentTimeMillis();
+        else
+            return;
 
-  public void draw(Canvas canvas) {
-    if (face != null) {
-      canvas.drawBitmap(face, x, y, paint);
+        /* 충돌검사 */
+        if ((x >= targetX() && x <= targetXWidth()) && y >= targetY() && y <= targetYHeight())
+            isHit = true;
+
+        /* 유도 알고리즘 */
+        if (targetCntrX() < x)
+        {
+            x -= speed;
+            if (targetCntrY() < y)
+                y -= speed;
+            else if (targetCntrY() > y)
+                y += speed;
+        }
+        if (targetCntrX() > x)
+        {
+            x += speed;
+            if (targetCntrY() < y)
+                y -= speed;
+            else if (targetCntrY() > y)
+                y += speed;
+        }
+        if (targetCntrX() == x)
+        {
+            if (targetCntrY() < y)
+                y -= speed;
+            else if (targetCntrY() > y)
+                y += speed;
+        }
+
     }
-  }
 
-  private int targetX() {
-    return GameState.getInstance().Mobs.get(target).x;
-  }
+    private int targetX()
+    {
+        return GameState.getInstance().Mobs.get(target).x;
+    }
 
-  private int targetY() {
-    return GameState.getInstance().Mobs.get(target).y;
-  }
+    private int targetY()
+    {
+        return GameState.getInstance().Mobs.get(target).y;
+    }
 
-  private int targetCntrX() {
-    return GameState.getInstance().Mobs.get(target).cntrX;
-  }
+    private int targetCntrX()
+    {
+        return GameState.getInstance().Mobs.get(target).cntrX;
+    }
 
-  private int targetCntrY() {
-    return GameState.getInstance().Mobs.get(target).cntrY;
-  }
+    private int targetCntrY()
+    {
+        return GameState.getInstance().Mobs.get(target).cntrY;
+    }
 
-  private int targetXWidth() {
-    return GameState.getInstance().Mobs.get(target).x
-        + GameState.getInstance().Mobs.get(target).width;
-  }
+    private int targetXWidth()
+    {
+        return GameState.getInstance().Mobs.get(target).x + GameState.getInstance().Mobs.get(target).width;
+    }
 
-  private int targetYHeight() {
-    return GameState.getInstance().Mobs.get(target).y
-        + GameState.getInstance().Mobs.get(target).height;
-  }
+    private int targetYHeight()
+    {
+        return GameState.getInstance().Mobs.get(target).y + GameState.getInstance().Mobs.get(target).height;
+    }
+
+    @Override
+    public void action()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void touch()
+    {
+        // TODO Auto-generated method stub
+    }
 }
